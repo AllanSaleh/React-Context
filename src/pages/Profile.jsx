3890abe67b1d1fdb43a5ad59492bc049faf4ed7a
Allan <allanahmedd@gmail.com>
2026-01-15 21:37:50 -0500

    profile user data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-
+import { useAuth } from "../contexts/AuthContext"
 // InClass Practice
 // Display logged in user in this page
 // Get user info from the Auth Context
 
 const Profile = () => {
+
+  const {user} = useAuth();
+
   return (
-    <div>Profile</div>
+    <div>
+      <h1>Profile Page</h1>
+      <h2>{user?.first_name || 'N/A'}</h2>
+      <hr />
+      <div>
+        <p>Last Name: {user?.lastName  || 'N/A'}</p>
+        <p>Email: {user?.email  || 'N/A'}</p>
+        <p>DOB: {user?.DOB  || 'N/A'}</p>
+        <p>Address: {user?.address  || 'N/A'}</p>
+        <p>Id: {user?.id  || 'N/A'}</p>
+      </div>
+    </div>
   )
 }
 
